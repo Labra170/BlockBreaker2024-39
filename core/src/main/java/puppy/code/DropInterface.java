f4a1a2307b71625d8b806aba51d761ec8b590798
Labra170 <78867359+Labra170@users.noreply.github.com>
2024-11-23 01:29:01 -0300

    Borrar interfaz en desuso

--- core/src/main/java/puppy/code/DropInterface.java
+++
@@ -1,7 +0,0 @@
-package puppy.code;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-
-// Interfaz para el Drop.
-public interface DropInterface {
-	public void Drop(ShapeRenderer shape, int x, int y, int width, int height, BlockBreakerGame aa);
-}
